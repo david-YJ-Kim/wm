320ee6092fb93bb7b03321e1368c732adba2646d
AzureAD\DavidKim <david@absolicsinc.com>
2024-07-20 14:16:31 -0400

    버전 체인지 테스트

--- src/main/resources/run_script_wm.sh
+++ src/main/resources/run_script_wm.sh
@@ -2,7 +2,7 @@
 # 프로세스 명을 명시한다.
 readonly PROC_NAME="wfs-wm-server"
 # jar 파일
-readonly DAEMON="../target/workman-?.?.?.jar"
+readonly DAEMON="../target/*.jar"
 
 readonly CONF="../config/application-server.yml"
 # 프로세스 아이디가 존재할 패스를 설정
@@ -19,7 +19,7 @@ start()
     else
 
         echo "/usr/lib/java/openjdk-8u342-b07/bin/java -jar -Dspring.config.additional-location=${CONF} -Dspring.profiles.active=server -Dname=${PROC_NAME} -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -Xms1024m -Xmx4096m -XX:MaxMetaspaceSize=256m -XX:MetaspaceSize=128m -XX:+UseG1GC -Dcom.sun.management.jmxremote.port=15021 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false "${DAEMON}""
-        nohup /usr/lib/java/openjdk-8u342-b07/bin/java -jar -Dspring.config.additional-location=${CONF} -Dspring.profiles.active=server -Dname=${PROC_NAME} -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -Xms1024m -Xmx4096m -XX:MaxMetaspaceSize=256m -XX:MetaspaceSize=128m -XX:+UseG1GC -Dcom.sun.management.jmxremote.port=15021 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false "${DAEMON}" > /dev/null 2>&1 &
+        nohup /usr/lib/java/openjdk-8u342-b07/bin/java -jar -Dspring.config.additional-location=${CONF} -Dspring.profiles.active=server -Dname=${PROC_NAME} -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -Xms1024m -Xmx4096m -XX:MaxMetaspaceSize=256m -XX:MetaspaceSize=128m -XX:+UseG1GC -Dcom.sun.management.jmxremote.port=15021 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false ${DAEMON} > /dev/null 2>&1 &
         local PID=${!}
 
         if [ -n ${PID} ]; then
